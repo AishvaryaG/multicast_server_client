fa227e4d3fedef04b93a20ad86ee1c9d587fdccb
Siddharth Manu <siddharthmanu@gmail.com>
2014-12-05 03:47:54 -0800

    refactored socket function

--- client.c
+++ client.c
@@ -12,35 +12,68 @@
 
 #include "common_hdr.h"
 
+
+/* Global data structures */
+int sockfd;
+
+/* Prototypes */
 static void *get_in_addr(struct sockaddr *sa);
 void sigint_handler(int s);
 void init_signal_handler();
+int init_server_socket(int argc, char *argv[]);
 
-int sockfd;
 
 int main(int argc, char *argv[])
 
 {
 	int numbytes;
-    struct addrinfo hints, *servinfo, *p;
-    int rv;
-
-    char s[INET6_ADDRSTRLEN];
 	char buf[MAXBUFFER];	
 
     init_signal_handler();
+
+	if (init_server_socket(argc, argv) != 0) {
+        fprintf(stderr,"Unable to setup server (socket, bind, listen)\n");
+        exit(1);
+	}
+
+    printf("Select the multicast group which you want to join as one of these %s : ", argv[3]);
+	scanf("%s", buf);
+    numbytes = pkt_send(sockfd, MSG_JOIN, buf,strlen(buf));
+
+    if (-1 == numbytes)
+        perror("send");
+
+	while (1) {
+		printf("Type a message. (Max 50 chars):\n");
+		scanf("%50s", buf);
+    	numbytes = pkt_send(sockfd, MSG_HELLO, buf, strlen(buf));
+    	if (-1 == numbytes) {
+        	perror("send");
+		}	
+	}	
+
+    return 0;
+}
+
+int init_server_socket(int argc, char *argv[]) 
+{
+    struct addrinfo hints, *servinfo, *p;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
+
     if (argc != 4) {
         fprintf(stderr,"usage: %s <server-port-num> <server-hostname>"
 				" <comma-separated-groups>\n",
 				argv[0]);
-        exit(1);
+       	return -1; 
     }
+
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     if ((rv = getaddrinfo(argv[2], argv[1], &hints, &servinfo)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        exit(1);
+		return -1;
     }
 
 	/* Any one of these could work! */
@@ -56,30 +89,16 @@ int main(int argc, char *argv[])
         }
         break;
     }
+
     if (p == NULL) {
         fprintf(stderr, "client: failed to connect\n");
-        exit(2);
+       	return -1; 
     }
     inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s, sizeof s);
     printf("client: connecting to %s\n", s);
     freeaddrinfo(servinfo); // all done with this structure
 
-    printf("Select the multicast group which you want to join as one of these %s : ", argv[3]);
-	scanf("%s", buf);
-    numbytes = pkt_send(sockfd, MSG_JOIN, buf,strlen(buf));
-
-    if (-1 == numbytes)
-        perror("send");
-
-	while (1) {
-		printf("Type a message. (Max 50 chars):\n");
-		scanf("%50s", buf);
-    	numbytes = pkt_send(sockfd, MSG_HELLO, buf, strlen(buf));
-    	if(-1 == numbytes)
-        	perror("send");
-	}	
-
-    return 0;
+	return 0;
 }
 
 static void *get_in_addr(struct sockaddr *sa)
@@ -110,5 +129,4 @@ void init_signal_handler()
         perror("sigaction sigint");
         exit(1);
     }   
-    
 }
